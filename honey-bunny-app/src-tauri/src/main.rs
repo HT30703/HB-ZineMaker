6c812a07c925a1e02abcff40fcd8055c151588ff
HT30703 <pokhei30703@gmail.com>
2026-08-06 14:41:52 +0100

    Add files via upload

--- honey-bunny-app/src-tauri/src/main.rs
+++ honey-bunny-app/src-tauri/src/main.rs
@@ -3,8 +3,6 @@
 
 fn main() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_fs::init())
         .run(tauri::generate_context!())
         .expect("error while running The Honey Bunny");
 }
